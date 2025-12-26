e6879f536eb568686d0fb8a16161b2ce98f44b8e
lastforever457 <lastforever457@gmail.com>
2025-12-26 12:10:21 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,22 @@ import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '1248',
-                pathname: '/**',
-            },
-        ],
-    },
-    
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1248',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.saminvestcompany.uz',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 const withNextIntl = createNextIntlPlugin()
