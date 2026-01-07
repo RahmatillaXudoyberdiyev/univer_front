1221abcdecd37e7270bb4a4d5d9f95075edf2089
rahmatillaxudoyberdiyev <xudoyberdiyevrahmatilla@gmail.com>
2026-01-07 22:04:47 +0500

    layout hydration error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import { Inter } from 'next/font/google'
-
 import ReactQueryProvider from '@/components/providers/react-query-provider'
 import ToastifyProvider from '@/components/providers/toastify-provider'
-import { NextIntlClientProvider } from 'next-intl'
+import { NextIntlClientProvider, useMessages } from 'next-intl'
 import './globals.css'
 
 const inter = Inter({
@@ -15,18 +14,22 @@ const defaultTheme: string = 'light'
 
 const Layout = ({
   children,
+  params: { locale }
 }: Readonly<{
   children: React.ReactNode
+  params: { locale: string }
 }>) => {
+  const messages = useMessages()
+
   return (
     <html
-      className={defaultTheme}
+      className={`${inter.variable} ${defaultTheme}`}
       style={{ colorScheme: defaultTheme }}
       suppressHydrationWarning
-      lang="uz"
+      lang={locale || "uz"}
     >
       <body className={`${inter.className} antialiased`}>
-        <NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <ReactQueryProvider>
             <ToastifyProvider />
             {children}
